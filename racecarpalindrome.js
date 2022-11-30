785a4f6f83df59f337d93ccb607dae81df780fb1
MangeshAwate <mawate9@gmail.com>
2022-11-30 19:05:05 +0530

    check

---
+++ racecarpalindrome.js
@@ -0,0 +1,13 @@
+
+str="rcaecar";
+
+let bag='';
+    for(i=N-1;i>=0;i--){
+        bag=bag+str[i];
+    }
+    if(str=bag){
+        console.log("Yes")
+    }
+    else{
+        console.log("No")
+    }
